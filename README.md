39452096f3371683b66fef59901088b1e38c36c2
Matt Helm <mthelm85@gmail.com>
2021-08-10 12:29:15 -0500

    added docstrings, readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # LaborAPI
+
+This package facilitates calling the DOL API. It defines a single function:
+
+```
+get(endpoint::String, params::Dict{String, Any})
+```
+
+Call the DOL API at the provided endpoint and with the provided parameters.
+The `params` dictionary must contain at a minimum your API key.
+
+# Examples
+
+```
+using LaborAPI
+
+params = Dict(
+    "X-API-KEY" => "adslfj3987234kjaf987fdas",
+    "fields" => "case_id, flsa_violtn_cnt"
+)
+
+endpoint = "whd/wh_enforcement"
+
+res = LaborAPI.get(endpoint, params)
+```
--- src/LaborAPI.jl
+++ src/LaborAPI.jl
@@ -4,7 +4,27 @@ using HTTP
 using JSON3
 
 const BASE_URL = "https://apiv3.dol.gov/v3/get/"
+"""
+    get(endpoint::String, params::Dict{String, Any})
 
+Call the DOL API at the provided endpoint and with the provided parameters.
+The `params` dictionary must contain at a minimum your API key.
+
+# Examples
+
+```
+using LaborAPI
+
+params = Dict(
+    "X-API-KEY" => "adslfj3987234kjaf987fdas",
+    "fields" => "case_id, flsa_violtn_cnt"
+)
+
+endpoint = "whd/wh_enforcement"
+
+res = LaborAPI.get(endpoint, params)
+```
+"""
 function get(endpoint::String, params::Dict{String, Any}) 
     url = BASE_URL * endpoint
     try
