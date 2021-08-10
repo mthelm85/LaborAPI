edb181d4beabbf50a36e913d7fabbd70dea5a741
Matt Helm <mthelm85@gmail.com>
2021-08-09 20:42:24 -0500

    added error handling for API call

--- src/LaborAPI.jl
+++ src/LaborAPI.jl
@@ -6,17 +6,26 @@ using StructTypes
 
 const BASE_URL = "https://apiv3.dol.gov/v3/get"
 
-function get(endpoint::String, params::Dict{String, Any})
+function get(endpoint::String, params::Dict{String, Any}) 
     url = BASE_URL * endpoint
-    res = HTTP.get(
-        url,
-        ["Content-Type" => "application/json"],
-        query=params,
-        require_ssl_verification=false
-    )
-    json_str = String(res.body)
-    JSON3.@generatetypes json_str :LaborAPITypes
-    return JSON3.read(json_str, LaborAPITypes.Root).data
+    try
+        res = HTTP.get(
+            url,
+            ["Content-Type" => "application/json"],
+            query=params,
+            require_ssl_verification=false
+        )
+        if HTTP.status(res) == 200
+            json_str = String(res.body)
+            JSON3.@generatetypes json_str :LaborAPITypes
+            return JSON3.read(json_str, LaborAPITypes.Root).data
+        else
+            return error("ERROR: Expected status code 200 but got status code: $(HTTP.status(res))")
+        end
+    catch e
+        return e
+    end
 end
+
 
 end
